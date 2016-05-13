8bf1da3ef872c5302a7f695802612d5daf08f705
Mauricio Desiderio <mdesiderio@appirio.com>
2016-05-13 16:29:00 -0500

    updates to make submission system run on mac

--- submission-system/env.sh
+++ submission-system/env.sh
@@ -1,5 +1,8 @@
 # The IP address of the computer running the submission system
 # For Windows and Mac OS
-#export IP=192.168.99.100
+export DOCKER_IP=192.168.99.100
+export HOST_IP=192.168.1.75
+
 # For Linux
-export IP=192.168.1.65
+#export DOCKER_IP=192.168.1.75
+#export HOST_IP=$DOCKER_IP
