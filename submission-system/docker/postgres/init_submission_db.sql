66bcba8d092fba92b90b31dc6311b45fb9b8c1ae
Guiqiang Zhang <skyhit@gmail.com>
2016-05-18 21:31:47 +0800

    update init sql and remove unecessory dependency

--- submission-system/docker/postgres/init_submission_db.sql
+++ submission-system/docker/postgres/init_submission_db.sql
@@ -1,11 +1,3 @@
-CREATE DATABASE test
-  WITH OWNER = postgres
-       ENCODING = 'UTF8'
-       TABLESPACE = pg_default
-       LC_COLLATE = 'en_US.utf8'
-       LC_CTYPE = 'en_US.utf8'
-       CONNECTION LIMIT = -1;
-\connect test;
 CREATE TABLE public.submission
 (
   id serial NOT NULL,
