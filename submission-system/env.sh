558dd6ae949ab4d710b70bc80f80a6b7a016677b
Mauricio Desiderio <mdesiderio@appirio.com>
2016-08-02 17:23:38 -0500

    adding project mock service to submission system

--- submission-system/env.sh
+++ submission-system/env.sh
@@ -1,8 +1,8 @@
 # The IP address of the computer running the submission system
 # For Windows and Mac OS
-#export DOCKER_IP=192.168.99.100
-#export HOST_IP=192.168.1.71
+export DOCKER_IP=192.168.99.100
+export HOST_IP=192.168.1.71
 
 # For Linux
-export DOCKER_IP=192.168.1.74
-export HOST_IP=$DOCKER_IP
+#export DOCKER_IP=192.168.1.74
+#export HOST_IP=$DOCKER_IP
