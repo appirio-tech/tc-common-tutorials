1db653bd4d14d4ca5e254b1fe1046716a4e0582a
FireIce <skyhit@gmail.com>
2016-08-18 03:45:26 +0000

    fix deploy.sh for autopilot

--- docker/online_review/docker/build/autopilot/deploy.sh
+++ docker/online_review/docker/build/autopilot/deploy.sh
@@ -6,7 +6,7 @@ SVN_PASSWORD=$2
 svn co --non-interactive --no-auth-cache --username $SVN_USERNAME --password $SVN_PASSWORD https://coder.topcoder.com/tcs/clients/cronos/applications/online_review/trunk /root/online_review
 mv build.properties /root/online_review/
 mv token.properties /root/online_review/
-mv build.properties.autopilot /root/online_review/scripts/auto_pilot/
+mv build.properties.autopilot /root/online_review/scripts/auto_pilot/build.properties
 #add svn user name and password to build.properies
 echo "build.svn.username=$SVN_USERNAME" >> /root/online_review/build.properties
 echo "build.svn.password=$SVN_PASSWORD" >> /root/online_review/build.properties
