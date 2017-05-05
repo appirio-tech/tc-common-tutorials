17f9dbeefd1a5452e5f30bdb5b0575011fa03f1b
Guiqiang Zhang <skyhit@gmail.com>
2017-05-05 12:42:23 +0800

    not overwrite token if exists

--- docker/online_review/docker/build/scripts/build-auto-pilot.sh
+++ docker/online_review/docker/build/scripts/build-auto-pilot.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 rm -rf /root/deployment/auto_pilot
 
-cp -f /root/config/online_review/build.properties.docker /root/online_review/build.properties
-cp -f /root/config/online_review/token.properties.docker /root/online_review/token.properties
+cp -n /root/config/online_review/build.properties.docker /root/online_review/build.properties
+cp -n /root/config/online_review/token.properties.docker /root/online_review/token.properties
 
-cp -f /root/config/auto_pilot/build.properties.docker /root/online_review/scripts/auto_pilot/build.properties
+cp -n /root/config/auto_pilot/build.properties.docker /root/online_review/scripts/auto_pilot/build.properties
 
 cd /root/online_review/scripts/auto_pilot
 ant clean dist deploy
--- docker/online_review/docker/build/scripts/build-online-review.sh
+++ docker/online_review/docker/build/scripts/build-online-review.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 rm -rf /root/deployment/jboss-4.0.2
 
-cp -f /root/config/online_review/build.properties.docker /root/online_review/build.properties
-cp -f /root/config/online_review/token.properties.docker /root/online_review/token.properties
+cp -n /root/config/online_review/build.properties.docker /root/online_review/build.properties
+cp -n /root/config/online_review/token.properties.docker /root/online_review/token.properties
 
 cd /root/deployment
 tar xzf /root/jboss-4.0.2.tar.gz
