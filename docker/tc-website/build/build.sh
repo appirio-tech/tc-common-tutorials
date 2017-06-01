8fd232f316bdf7880d972d1fac9b81b72b47b147
liuliquan <leviastan@gmail.com>
2017-06-01 10:25:37 +0800

    TC WEBSITE - MARATHON MATCH UPDATE

--- docker/tc-website/build/build.sh
+++ docker/tc-website/build/build.sh
@@ -15,10 +15,12 @@ cp -f /root/files/TC.prod.ldap.keystore $JBOSS_HOME/bin
 cp -f /root/files/resources/paymentRanges.xml $JBOSS_HOME/server/all/conf
 
 # init code
+cp -f /root/files/build_mm.xml /root/tc-platform/tc-website
 cp -f /root/files/resources/ApplicationServer.properties /root/tc-platform/tc-website/resources
 cp -f /root/files/distui/jboss-web.xml /root/tc-platform/tc-website/resources/distui
 
 # deploy
 cd /root/tc-platform/tc-website
+ant -f build_mm.xml deploy
 ant -f build_tc.xml clean deploy
 ant -f build_distui.xml clean deploy
