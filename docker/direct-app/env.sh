2738e2f39caa5387d5719fa7d8a1c3a0ceedb7a3
guily-langa <34401880+guily-langa@users.noreply.github.com>
2017-12-10 00:41:38 +0200

    Update env.sh

--- docker/direct-app/env.sh
+++ docker/direct-app/env.sh
@@ -1,3 +1,3 @@
-export BUILT_DATA=<<path to a clean deploy directory>>
-export DIRECT_APP_SRC_ROOT=<<path to direct app>>
+export BUILT_DATA=C:\Users\Guily\Documents\GitHub\direct-app
+export DIRECT_APP_SRC_ROOT=https://github.com/appirio-tech/direct-app
 export BUILT_JBOSS_DIR=$BUILT_DATA/jboss-4.2.3.GA
